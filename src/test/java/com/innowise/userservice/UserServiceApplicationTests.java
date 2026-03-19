860cb19ebd0b6396102cc5523f1babab403a979b
travka677 <danikkim19@gmail.com>
2026-03-19 15:39:51 +0300

    comment UserServiceApplicationTests

--- src/test/java/com/innowise/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/innowise/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.innowise.userservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.innowise.userservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UserServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
